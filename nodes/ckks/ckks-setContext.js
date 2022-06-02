8544e43cbcbf6e50a8c0113ae676ba475a5e8ddd
Nataset Tanabodee <nataset.t@ku.th>
2022-06-02 20:44:57 +0700

    rm copy

--- nodes/ckks/ckks-setContext.js
+++ nodes/ckks/ckks-setContext.js
@@ -8,7 +8,10 @@ module.exports = function (RED) {
 
         node.on('input', async function (msg) {
             const seal = await SEAL();
-            const { parmsBase64, pkBase64, rlkBase64, scale } = msg.payload;
+            const { parmsBase64, pkBase64, rlkBase64, scale } = JSON.parse(msg.payload);
+            Object.keys(msg.payload, key => {
+                console.log(key);
+            });
             const parms = seal.EncryptionParameters();
             parms.load(parmsBase64);
 
@@ -19,18 +22,19 @@ module.exports = function (RED) {
             public_key.load(context, pkBase64);
             relin_key.load(context, rlkBase64);
 
-            const encryptor = seal.Encryptor(context, public_key);
-            const evaluator = seal.Evaluator(context);
-            const encoder = seal.Encoder(context);
+            // const encryptor = seal.Encryptor(context, public_key);
+            // const evaluator = seal.Evaluator(context);
+            // const encoder = seal.Encoder(context);
 
-            flowContext.set('publicKey', public_key);
-            flowContext.set('relinKey', relin_key);
-            flowContext.set('context', context);
-            flowContext.set('encryptor', encryptor);
-            flowContext.set('evaluator', evaluator);
-            flowContext.set('scale', scale);
+            // flowContext.set('publicKey', public_key);
+            // flowContext.set('relinKey', relin_key);
+            // flowContext.set('context', context);
+            // flowContext.set('encryptor', encryptor);
+            // flowContext.set('evaluator', evaluator);
+            // flowContext.set('encoder', encoder);
+            // flowContext.set('scale', Math.pow(2, scale));
 
-            msg.payload = 'Context already setted up';
+            // msg.payload = 'Context already setted up';
         });
     }
 
