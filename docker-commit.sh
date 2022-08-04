fbec3b092a6cfe6bea95d805a91210cc616400dd
Nataset <nataset.t@ku.th>
2022-08-05 03:24:45 +0700

    rm docker-commit.sh

--- docker-commit.sh
+++ docker-commit.sh
@@ -3,6 +3,6 @@ docker rm -f commit
 docker run -d -it -p 2000:1880 --name commit nodered/node-red:2.2.2
 docker exec -u 0 commit rm -fr /data
 docker cp /home/nataset/dev/cipherflow commit:/data
-docker commit commit nataset/node-red-cipherflow
-docker push nataset/node-red-cipherflow
+docker commit commit nataset/node-red-cipherflow:0.1.4_amd64
+docker push nataset/node-red-cipherflow:0.1.4_amd64
 docker rm -f commit 
